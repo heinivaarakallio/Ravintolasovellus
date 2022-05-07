82823eb195e17832a7a591c316333d134104fe56
heinivaarakallio <heini.vaarakallio@helsinki.fi>
2022-05-07 20:56:48 +0300

    Added tables to sql

--- schema.sql
+++ schema.sql
@@ -2,3 +2,19 @@ CREATE TABLE restaurants (
     id SERIAL PRIMARY KEY,
     content TEXT
 );
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    role INTEGER
+);
+CREATE TABLE opening_hours (
+    id SERIAL PRIMARY KEY,
+    restaurant_id INTEGER REFERENCES restaurants
+);
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    restaurant_id INTEGER REFERENCES restaurants,
+    user_id INTEGER REFERENCES users,
+    review TEXT
+);
