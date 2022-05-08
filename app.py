006d27c621f36886b03ab4532b77a64f26e63907
heinivaarakallio <heini.vaarakallio@helsinki.fi>
2022-05-08 17:48:10 +0300

    Sign up page done

--- app.py
+++ app.py
@@ -26,14 +26,6 @@ def logout():
     del session["username"]
     return redirect("/")
 
-@app.route("/page1")
-def page1():
-    return "Tämä on sivu 1"
-
-@app.route("/page2")
-def page2():
-    return "Tämä on sivu 2"
-
 @app.route("/form")
 def form():
     return render_template("form.html")
@@ -41,3 +33,33 @@ def form():
 @app.route("/result", methods=["POST"])
 def result():
     return render_template("result.html", name=request.form["name"])
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    if request.method == "POST":
+        errormessage = ""
+
+        username = request.form["username"]
+        if len(username) == 0:
+            errormessage = "Käyttäjätunnuksen luominen ei onnistunut. Käyttäjätunnus ei saa olla tyhjä."
+
+        password = request.form.getlist("password")
+        if len(password[0]) == 0:
+            errormessage = "Käyttäjätunnuksen luominen ei onnistunut. Salasana ei saa olla tyhjä."
+        if password[0] != password[1]:
+            errormessage = "Käyttäjätunnuksen luominen ei onnistunut. Salasanat eivät täsmää."
+
+        role = request.form["role"]
+        if role != "1" and role != "2":
+            errormessage = "Käyttäjätunnuksen luominen ei onnistunut. Valitse joko peruskäyttäjän tai ylläpitäjän rooli."
+
+        if len(errormessage) > 0:
+            return render_template("signup.html", errormessage=errormessage)
+
+        username = request.form["username"]
+        password = request.form["password"]
+        session["username"] = username
+        return redirect("/")
+
